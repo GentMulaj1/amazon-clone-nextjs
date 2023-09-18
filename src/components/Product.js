ad309df99a0b942830456df8fe592fce963bfa60
GentMulaj1 <mulajgent2001@gmail.com>
2023-09-18 22:02:53 +0200

    fix-npm-v-18-and-react-currency-formatter

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import { StarIcon } from "@heroicons/react/solid"
-import Currency from "react-currency-formatter"
 
 const Product = ({ id, title, price ,description, category, image }) => {
 
@@ -14,11 +13,13 @@ const Product = ({ id, title, price ,description, category, image }) => {
 
     const [hasPrime] = useState(Math.random() < 0.5);
 
-    useEffect(() => {
-        // This code will run on the client side
-      }, []);
+    let dollarUS = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
 
-  return (
+
+    return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
 
         <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
@@ -38,7 +39,8 @@ const Product = ({ id, title, price ,description, category, image }) => {
         <p className="text-xs my-2 line-clamp-2 ">{description}</p>
 
         <div className="mb-5 ">
-            <Currency quantity={price} currency="USD"/>
+            {dollarUS.format(price)}
+
         </div>
 
 
